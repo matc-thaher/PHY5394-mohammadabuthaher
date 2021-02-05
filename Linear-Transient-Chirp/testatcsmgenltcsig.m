831fc1758fcf7d8ba9e9621a8ef30df4e0bd3efd
Mohammad Abu Thaher Chowdhury <atc.nasim@gmail.com>
2021-02-04 19:44:34 -0600

    Error corrected: name of function, timeData, maximum instantaneous frequency

--- Linear-Transient-Chirp/testatcsmgenltcsig.m
+++ Linear-Transient-Chirp/testatcsmgenltcsig.m
@@ -1,18 +1,18 @@
 %% Plot the linear transient chirp signal
 % Signal parameters
-A = 10;
+A = 5;
 t_a = 0.7;
-L = 10;
-f_0 = 5;
-f_1 = 7;
+L = 3;
+f_0 = 2;
+f_1 = 5;
 phase = 0;
 % Maximum instantaneous frequency after t_a + L sec is
 maxinstFreq = f_0 + 2 * f_1 * L;
-samplFreq = maxinstFreq/20;
+samplFreq = 5 * maxinstFreq;
 samplIntrvl = 1/samplFreq;
 
 % Time samples
-timedata = -20:samplIntrvl:20;
+timedata = -5:samplIntrvl:5;
 % Number of samples
 nSamples = length(timedata);
 % Generate the signal
@@ -20,7 +20,7 @@ sigVec = atcsmgenltcsig(timedata,[t_a, t_a + L], A,[f_0,f_1], phase);
 %Plot the signal 
 figure;
 plot(timedata,sigVec, '* -')
-xlabel("Time")
+xlabel("Time in sec")
 ylabel("s(t)")
 title("Linear Transient Chirp Signal")
 
