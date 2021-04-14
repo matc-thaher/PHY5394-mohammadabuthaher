21c3c93cbb8955ab0861870217aeee013fe09275
Mohammad Abu Thaher Chowdhury <atc.nasim@gmail.com>
2021-04-14 08:37:14 -0500

    Plot the tmeseries and periodogram of LTC

---
+++ Final labs/Lab 1/Task 1/testatcsmgenltcsignew.m
@@ -0,0 +1,48 @@
+%% Plot the linear transient chirp signal
+% Signal parameters
+A = 10;
+t_a = 0.2;
+L = 0.6;
+f_0 = 50;
+f_1 = 70;
+phase = 0;
+
+% parameters
+nSamples = 2048;
+samplFreq = 2048;
+% Time samples
+timedata = (0:nSamples-1)/samplFreq;
+
+% struct of the vectors
+sigTime = struct('sTsig', t_a, 'fTsig', t_a+L);
+sigFreq = struct('inFreq', f_0, 'fnFreq', f_1);
+
+% Generate the signal
+sigVec = atcsmgenltcsignew(timedata,sigTime,A,sigFreq,phase);
+
+%% Plots
+% Plot the time series 
+figure;
+plot(timedata,sigVec, '* -')
+xlabel("Time in sec")
+ylabel("s(t)")
+title("Linear Transient Chirp Signal")
+
+% Parameters for the Periodogram
+% Length of data 
+dataLen = timedata(end)-timedata(1);
+% DFT sample corresponding to Nyquist frequency
+kNyq = floor(nSamples/2)+1;
+% Positive Fourier frequencies
+posFreq = (0:(kNyq-1))*(1/dataLen);
+% FFT of signal
+fftSig = fft(sigVec);
+% Discard negative frequencies
+fftSig = fftSig(1:kNyq);
+
+%Plot periodogram
+figure;
+plot(posFreq,abs(fftSig), 'm -');
+xlabel("Frequency in Hz(only positive value)")
+ylabel("Magnitude of signal")
+title("Periodogram of Linear Transient Chirp Signal")
