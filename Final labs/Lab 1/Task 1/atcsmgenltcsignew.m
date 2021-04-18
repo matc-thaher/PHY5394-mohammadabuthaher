d141d6b6c9a30babc055f66cfb2844621a2696f2
Mohammad Abu Thaher Chowdhury <atc.nasim@gmail.com>
2021-04-17 21:06:40 -0500

    Generate a LTC with struct input

--- Final labs/Lab 1/Task 1/atcsmgenltcsignew.m
+++ Final labs/Lab 1/Task 1/atcsmgenltcsignew.m
@@ -1,4 +1,4 @@
-function sigVec = atcsmgenltcsignew(timeData,timeSteps,snr, freq, phase)
+function sigVec = atcsmgenltcsignew(timeData,snr, sigParameters)
 % Generate a linear transient chirp signal
 % S = ATCSMGENLTCSIG(T,TS,SNR,FQ,P)
 % Generates a linear transient chirp signal S. T is the vector of
@@ -11,9 +11,9 @@ function sigVec = atcsmgenltcsignew(timeData,timeSteps,snr, freq, phase)
 
 %Mohammad Abu Thaher Chowdhury, April 2021
 
-idxt = find(timeData>=timeSteps.sTsig & timeData <= timeSteps.fTsig);
+idxt = find(timeData>=sigParameters.sTsig & timeData <= sigParameters.fTsig);
 sigVec = zeros(1, length(timeData));
-phaseVec = 2 * pi * (freq.inFreq * (timeData(idxt) - timeSteps.sTsig) + freq.fnFreq * (timeData(idxt) - timeSteps.sTsig).^2) + phase;
+phaseVec = 2 * pi * (sigParameters.inFreq * (timeData(idxt) - sigParameters.sTsig) + sigParameters.fnFreq * (timeData(idxt) - sigParameters.sTsig).^2) + sigParameters.phase;
 trigSin = sin(phaseVec);
 sigVec(idxt) = snr * trigSin/norm(trigSin);
 
