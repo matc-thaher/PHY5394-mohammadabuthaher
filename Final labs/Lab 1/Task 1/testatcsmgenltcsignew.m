45e165978cef6d981c60298d6e650e4c9e7915bd
Mohammad Abu Thaher Chowdhury <atc.nasim@gmail.com>
2021-04-17 21:50:34 -0500

    Test script for LTC signal

--- Final labs/Lab 1/Task 1/testatcsmgenltcsignew.m
+++ Final labs/Lab 1/Task 1/testatcsmgenltcsignew.m
@@ -1,11 +1,11 @@
 %% Plot the linear transient chirp signal
 % Signal parameters
 A = 10;
-t_a = 0.2;
-L = 0.6;
-f_0 = 50;
-f_1 = 70;
-phase = 0;
+%t_a = 0.2;
+%L = 0.6;
+%f_0 = 50;
+%f_1 = 70;
+%phase = 0;
 
 % parameters
 nSamples = 2048;
@@ -14,7 +14,8 @@ samplFreq = 2048;
 timedata = (0:nSamples-1)/samplFreq;
 
 % struct of the vectors
-sigParameters = struct('sTsig', t_a, 'fTsig', t_a + L, 'inFreq', f_0, 'fnFreq', f_1, 'phase', 0);
+%sigParameters = struct('sTsig', t_a, 'fTsig', t_a + L, 'inFreq', f_0, 'fnFreq', f_1, 'phase', 0);
+sigParameters = struct('sTsig', 0.2, 'fTsig', 0.8, 'inFreq', 50, 'fnFreq', 70, 'phase', 0);
 % Generate the signal
 sigVec = atcsmgenltcsignew(timedata,A, sigParameters);
 
