82d8a7e653dfc9701f0cb2729c8e1da27979ee08
Mohammad Abu Thaher Chowdhury <atc.nasim@gmail.com>
2021-04-17 21:52:00 -0500

    Test scripts for Function handle

--- Final labs/Lab 1/Task 2/testatcsmgenltcsigncs.m
+++ Final labs/Lab 1/Task 2/testatcsmgenltcsigncs.m
@@ -3,12 +3,11 @@
 addpath '../../Lab 1/Task 1'
 
 % Signal parameters
-A = 5;
-t_a = 0.2;
-L = 0.6;
-f_0 = 50;
-f_1 = 70;
-phase = 0;
+%t_a = 0.2;
+%L = 0.6;
+%f_0 = 50;
+%f_1 = 70;
+%phase = 0;
 
 % parameters
 nSamples = 2048;
@@ -19,7 +18,8 @@ timedata = (0:nSamples-1)/samplFreq;
 % struct of the vectors
 %sigTime = struct('sTsig', t_a, 'fTsig', t_a+L);
 %sigFreq = struct('inFreq', f_0, 'fnFreq', f_1);
-sigParameters = struct('sTsig', t_a, 'fTsig', t_a + L, 'inFreq', f_0, 'fnFreq', f_1, 'phase', 0);
+%sigParameters = struct('sTsig', t_a, 'fTsig', t_a + L, 'inFreq', f_0, 'fnFreq', f_1, 'phase', 0);
+sigParameters = struct('sTsig', 0.2, 'fTsig', 0.8, 'inFreq', 50, 'fnFreq', 70, 'phase', 0);
 snr = struct('snr1', 10, 'snr2', 12, 'snr3', 15);
 
 % Generate the signal using function handle
